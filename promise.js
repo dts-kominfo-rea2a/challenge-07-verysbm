185e8346630f1022fc3b2dc00dfe51da571dd7ec
verysetiabudi <verysetiabudi37@gmail.com>
2022-06-12 23:00:05 +0700

    feat: challenge

--- promise.js
+++ promise.js
@@ -1,8 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let theaterIXX = await promiseTheaterIXX();
+  let theaterVGC = await promiseTheaterVGC();
+  let theater = theaterIXX.concat(theaterVGC);
 
+  let result = theater.filter((item) => item.hasil === emosi);
+  return result.length;
+};
+
+//2
+//4
 module.exports = {
   promiseOutput,
 };
